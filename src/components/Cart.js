66b9fbf43c07573ba6f1085bbf39f75ec76ff94b
DhanushM12 <dhanushmukhi@gmail.com>
2021-01-21 18:11:20 +0530

    added the cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import Cartitem from './Cartitem'
 
-function Cart() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const Cart = (props) => {
+  const { books } = props;
+  return (
+    <div className="cart">
+      {books.map((book) => {
+        return (
+          <Cartitem
+            book={book}
+            key={book.bookID}
+            onIncreaseQuantity={props.onIncreaseQuantity}
+            onDecreaseQuantity={props.onDecreaseQuantity}
+            onDeleteProduct={props.onDeleteProduct}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default Cart;
+
